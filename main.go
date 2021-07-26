467a16b2e651d4911b83fef7e53615002a74966e
Asianatix <56123789+Asianatix@users.noreply.github.com>
2021-07-26 20:45:59 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 	"strings"
 	"time"
 
-	"github.com/penggy/EasyGoLib/db"
+	"github.com/Asianatix/EasyGoLib/db"
 
 	"github.com/EasyDarwin/EasyDarwin/models"
 	"github.com/EasyDarwin/EasyDarwin/routers"
 	"github.com/EasyDarwin/EasyDarwin/rtsp"
 	figure "github.com/common-nighthawk/go-figure"
-	"github.com/penggy/EasyGoLib/utils"
-	"github.com/penggy/service"
+	"github.com/Asianatix/EasyGoLib/utils"
+	"github.com/Asianatix/service"
 )
 
 var (
